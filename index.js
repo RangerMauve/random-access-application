a91a0acfeae97786e73835fe5d76683cdc20b7b7
mauve <ranger@mauve.moe>
2022-12-23 15:54:24 -0500

    Use new for RAF constructor

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const envPaths = require('env-paths')
-const randomAccessFile = require('random-access-file')
+const RAF = require('random-access-file')
 const path = require('path')
 
 module.exports = function RAA (application) {
@@ -9,5 +9,5 @@ module.exports = function RAA (application) {
 
   const dir = paths.data
 
-  return (file, ...options) => randomAccessFile(path.join(dir, file), ...options)
+  return (file, ...options) => new RAF(path.join(dir, file), ...options)
 }
